40445d59137df7692e7b6937b80e898d82aa9866
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-09-09 21:59:45 -0500

    feat: add media file handling by configuring MEDIA_URL and MEDIA_ROOT, and enable static serving in debug mode

--- config/settings.py
+++ config/settings.py
@@ -61,6 +61,7 @@ CKEDITOR_5_CONFIGS = {
             "bold",
             "italic",
             "link",
+            "imageUpload",
             "bulletedList",
             "numberedList",
             "blockQuote",
@@ -152,6 +153,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Email

98fbca2fe04489e89b1862dfe3bfe6ca97247e9c
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-09-22 20:17:17 -0500

    feat: message on successful sign up

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import login, logout
+from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.shortcuts import redirect, render
 from django.utils.http import url_has_allowed_host_and_scheme
@@ -30,10 +31,14 @@ def login_view(request):
 
 
 def signup(request):
-	form = SignupForm(request.POST or None)
+	form = UserCreationForm(request.POST or None)
 	if request.method == "POST" and form.is_valid():
 		user = form.save()
 		login(request, user)
+		messages.success(
+			request,
+			f"You're signed up, {user.username}! Welcome to compile/.",
+		)
 		return _redirect_after_auth(request)
 
 	return render(

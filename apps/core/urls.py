8b5b7a8e1930b4fba5a0a8767982bbab9444c851
Phillip Cole <42485975+philquake@users.noreply.github.com>
2026-09-23 00:55:46 -0500

    fix: ads.txt url routing

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
+from pathlib import Path
 from .views import home, privacy, terms
+from django.http import FileResponse
 
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+def ads_txt(request):
+    return FileResponse(
+        open(BASE_DIR / "ads.txt", "rb"),
+        content_type="text/plain"
+    )
+    
 urlpatterns = [
     path("", home, name="home"),
     path("terms/", terms, name="terms"),
     path("privacy/", privacy, name="privacy"),
+    path("ads.txt", ads_txt),
 ]
+
